7fbe5b72214699c282b1a339ae02acb4caa5810c
git <mhspecialitycoffee@gmail.com>
2022-01-19 12:09:12 +0100

    moving eyes

---
+++ js/eyes.js
@@ -0,0 +1,13 @@
+var  eye = document.getElementsByClassName("eye-ball");
+document.onmousemove = function(e){
+var width = window.innerWidth;
+var x = e.pageX;// event.clientX;
+var a = x / width;
+a= a *100;
+var height = window.innerHeight;
+var y = e.pageY;//event.clientY;
+var b = y / height;
+b= b *100;
+eye[0].style.cssText = "left: " + a + "%; top: " + b + "%;" + "transform:translate(-"+a+"%,-"+b+"%)";
+eye[1].style.cssText = "left: " + a + "%; top: " + b + "%;" + "transform:translate(-"+a+"%,-"+b+"%)"; 
+}
